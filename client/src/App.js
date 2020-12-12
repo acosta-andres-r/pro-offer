6aa896ffb7936cac0c107a99b15a56aa3c9a2573
Andres A <acosta.andres.r@gmail.com>
2020-12-12 12:00:46 -0600

    Included routes to test live chat.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ProductDB from "./container/ProductDB";
+import Chat from "./container/Chat";
+import Admin from "./container/Admin"
 
 function App() {
 
   return (
-    <ProductDB />
+    // <ProductDB />
+
+    <Router>
+      <Switch>
+        <Route exact path={"/"}>
+          <Chat />
+        </Route>
+        <Route exact path={"/admin"}>
+          <Admin />
+        </Route>
+        {/* <Route exact path="/books/:id">
+            <Detail />
+          </Route> */}
+        {/* <Route>
+      <NoMatch />
+    </Route> */}
+      </Switch>
+    </Router >
+
+
   );
 }
 
 export default App;
+
+
