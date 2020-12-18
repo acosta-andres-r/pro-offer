c8c57da43df5c6bfbcea7f0f65c7b8776d1f9e2c
Andres A <acosta.andres.r@gmail.com>
2020-12-17 20:59:11 -0600

    Worked on search history continuity

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -5,7 +5,6 @@ import API from '../utils/API'
 
 import List from '../components/Lists/List'
 import Cards from '../components/Cards/Cards'
-import Aux from '../components/hoc/Aux/Aux'
 
 const classes = {
   flexGrow: 2,
@@ -15,27 +14,60 @@ const classes = {
 };
 
 const buy = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-  const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchNames, setSearchNames] = useState(['Camera', 'Boiler'])
 
   const { id } = useParams()
 
   useEffect(() => {
-    // console.log('Entered in readAllProduct');
     API.getProducts()
       .then(res => {
-        // console.log(id);
-        const searchedProducts = res.data.filter((product) => {
-          if (product.description) {
-            return product.description.toLowerCase().includes(id.toLowerCase())
-          }
-          return product.title.toLowerCase().includes(id.toLowerCase())
-        })
-        setProducts(searchedProducts);
+         setAllProducts(res.data);
       })
       .catch(err => console.log(err));
 
   }, [])
+
+  useEffect(() => {
+    const savedNames = JSON.parse(localStorage.getItem('history')) || searchNames;
+    const nameFound = savedNames.findIndex(name => {
+      return name.toLocaleLowerCase() === id.toLocaleLowerCase()
+    })
+    if (nameFound === -1) {
+      setSearchNames([...savedNames, id]);
+      setSelectedIndex(savedNames.length);
+      filterProducts(allProducts, id);
+      handleListItemClick(savedNames.length, id);
+    } else {
+      setSearchNames([...savedNames]);
+      setSelectedIndex(nameFound);
+      filterProducts(allProducts, id);
+      handleListItemClick(nameFound, id);
+    }
+  }, [allProducts])
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('history', JSON.stringify(searchNames))
+    }
+  })
+  const filterProducts = (array, search) => {
+    const searchedProducts = array.filter((product) => {
+      if (product.description) {
+        return product.description.toLowerCase().includes(search.toLowerCase())
+      }
+      return product.title.toLowerCase().includes(search.toLowerCase())
+    })
+    setFilteredProducts(searchedProducts);
+  };
+
+  const handleListItemClick = (index, name) => {
+    setSelectedIndex(index);
+    filterProducts(allProducts, name);
+  };
 
   const diplayProductInfoHandler = (event) => {
     console.log(`show product ${event.target.getAttribute('data-id')}`);
@@ -43,8 +75,11 @@ const buy = () => {
 
   return (
     <div style={classes}>
-      <List />
-      <Cards productData={products} clicked={diplayProductInfoHandler} />
+      <List
+        selectedIndex={selectedIndex}
+        searchNames={searchNames}
+        handleListItemClick={handleListItemClick} />
+      <Cards productData={filteredProducts} clicked={diplayProductInfoHandler} />
     </div>
   )
 };
