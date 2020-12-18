93a12b11693a5db22ba38b9c4ffad4d6b88fbd95
Andres A <acosta.andres.r@gmail.com>
2020-12-17 18:33:46 -0600

    Added AppBar with route for home and search products

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ProductDB from "./container/ProductDB";
 import Chat from "./container/Chat";
 import Admin from "./container/Admin"
+import Buy from "./pages/Buy"
+import Search from "./pages/Search"
+
+import AppBar from './components/AppBar/AppBar'
 
 function App() {
 
   return (
-    // <ProductDB />
-
     <Router>
-      <Switch>
-        <Route exact path={"/"}>
-          <Chat />
-        </Route>
-        <Route exact path={"/admin"}>
-          <Admin />
-        </Route>
-        {/* <Route exact path="/books/:id">
-            <Detail />
-          </Route> */}
-        {/* <Route>
+      <AppBar />
+      <div className={'Container'}>
+        <Switch>
+          <Route exact path={"/"}>
+            <Buy />
+          </Route>
+          <Route exact path={"/sell"}>
+            {/* <Sell /> */}
+          </Route>
+          <Route exact path={"/offers"}>
+            {/* <Sell /> */}
+          </Route>
+          <Route exact path={"/search/:id"}>
+            <Search />
+          </Route>
+          {/* <Route>
       <NoMatch />
     </Route> */}
-      </Switch>
+        </Switch>
+      </div>
     </Router >
-
-
   );
 }
 
